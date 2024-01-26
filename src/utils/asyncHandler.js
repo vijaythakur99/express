35ff5ce341e1389f632812c2f33f1e6b6ad7dee6
Vijay Pratap <vijaycalmpratap17.com>
2024-01-26 23:12:01 +0530

    add async handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise
+        .resolve(requestHandler(req, res, next))
+        .catch(err => next(err))
+    }
+}
+
+export { asyncHandler };
+
+
+/*
+//alternate appraoch
+const asyncHandler2 = requestHandler => async (req, res, next) => {
+    try {
+        await requestHandler(req, res, next);
+    } catch (error) {
+        res.status(error.code || 500)
+        .json({
+            success: false,
+            message: error.message ?? ''
+        })
+    }
+}
+*/
