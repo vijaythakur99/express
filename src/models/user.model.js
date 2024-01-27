4f122e6ecb3b43d18532c70d02cc6532b23b5db8
Vijay Pratap <vijaycalmpratap17.com>
2024-01-27 22:40:22 +0530

    add user model schema

---
+++ src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },    
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //cloudnary url service
+        required: true
+    },    
+    coverImage: {
+        type: String, //cloudnary url service
+    },
+    watchHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Video'
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String,
+    }
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema);
